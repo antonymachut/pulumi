daef558db89e374ae03e53a62929eae0278f9502
antony.machut <antony.machut@sernays.fr>
2024-11-18 22:14:48 +0100

    First blood

--- quickstart/__main__.py
+++ quickstart/__main__.py
@@ -2,6 +2,14 @@
 
 import pulumi
 from pulumi_gcp import storage
+import os
+import json
+
+google_credentials = os.getenv("GOOGLE_CREDENTIALS")
+if google_credentials:
+    credentials = json.loads(google_credentials)
+    # Configure les authentifications si nécessaire
+
 
 # Create a GCP resource (Storage Bucket)
 bucket = storage.Bucket('my-bucket', location="US")
