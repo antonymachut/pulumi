04d80f0d3a09d45844b8912ff60f2583aa16f7d5
antony.machut <antony.machut@sernays.fr>
2024-11-18 22:32:44 +0100

    wip

--- quickstart/__main__.py
+++ quickstart/__main__.py
@@ -1,21 +1,10 @@
 """A Google Cloud Python Pulumi program"""
 
 import pulumi
+import pulumi_gcp as gcp
 from pulumi_gcp import storage
-import os
-import base64
-import json
 
-encoded_credentials = os.getenv("GOOGLE_CREDENTIALS")
-if encoded_credentials:
-    decoded_credentials = base64.b64decode(encoded_credentials).decode("utf-8")
-    credentials = json.loads(decoded_credentials)
-    print("Service Account chargé avec succès !")
-else:
-    raise ValueError("La variable d'environnement GOOGLE_CREDENTIALS est introuvable.")
-
-
-
+gcp.config.credentials = pulumi.Config().require_secret("gcp:credentials")
 
 # Create a GCP resource (Storage Bucket)
 bucket = storage.Bucket('my-bucket', location="US")
